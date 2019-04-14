387311bd766e20c51aae4ee1404d084b2aba959f
Harystolho <haristolhogamer@gmail.com>
2019-04-13 21:40:41 -0300

    Preact development server works

---
+++ frontend/src/components/app.js
@@ -0,0 +1,11 @@
+import {Component} from 'preact';
+
+export default class App extends Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return <div></div>;
+    }
+}
