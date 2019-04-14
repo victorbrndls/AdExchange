a0d2dce5a8fec168bb9cb7ce7489c346a71887b0
Harystolho <haristolhogamer@gmail.com>
2019-04-14 19:42:07 -0300

    Spring runs on 8080 and Preact on 8081

--- backend/src/main/java/com/harystolho/adexchange/Main.java
+++ backend/src/main/java/com/harystolho/adexchange/Main.java
@@ -1,9 +1,14 @@
 package com.harystolho.adexchange;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
 public class Main {
 
 	public static void main(String[] args) {
-		
+		ApplicationContext ctx = SpringApplication.run(Main.class, args);
 	}
 
 }
