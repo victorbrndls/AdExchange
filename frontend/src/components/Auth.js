461312c484eaf662cf646f0d66cbe7b585e7c5e9
Harystolho <haristolhogamer@gmail.com>
2019-04-15 19:58:23 -0300

    Change Auth state on button click

--- frontend/src/components/Auth.js
+++ frontend/src/components/Auth.js
@@ -1,18 +1,40 @@
 import {Component} from "preact";
-import {Match} from 'preact-router/match'
+import {route} from 'preact-router';
 
 export default class Header extends Component {
-    constructor() {
+    constructor({url}) {
         super();
+
+        this.updateAuthMode(url);
     }
 
-    render({url}) {
+    componentWillUpdate({url}) {
+        this.updateAuthMode(url);
+    }
+
+    updateAuthMode = (url) => {
+        if (url.includes('register')) {
+            this.setState({mode: 'sign-up'});
+        } else {
+            this.setState({mode: 'sign-in'});
+        }
+    };
+
+    render(props, {mode}) {
         return (
             <div style="display: flex; justify-content: center;">
                 <div id="auth" class="shadow mt-5">
                     <div class="auth-sign-container">
-                        <div class="auth-sign">Sign In</div>
-                        <div class="auth-sign">Sign Up</div>
+                        <div class={"auth-sign " + (mode === 'sign-in' ? " active" : "")}
+                             style="border-radius: .25rem 0 0 0" onClick={() => {
+                            this.setState({mode: 'sign-in'});
+                        }}>Sign In
+                        </div>
+                        <div class={"auth-sign " + (mode === 'sign-up' ? " active" : "")}
+                             style="border-radius: 0 .25rem 0 0" onClick={() => {
+                            this.setState({mode: 'sign-up'});
+                        }}>Sign Up
+                        </div>
                     </div>
                     <div style="margin: 12px;">
                         <form class="text-center">
