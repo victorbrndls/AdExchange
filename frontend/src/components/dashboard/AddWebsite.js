4cd41317b368bb955f682ac8cd1d2c5e11e110a8
Harystolho <haristolhogamer@gmail.com>
2019-04-22 16:06:12 -0300

    Add categories to website creation process

--- frontend/src/components/dashboard/AddWebsite.js
+++ frontend/src/components/dashboard/AddWebsite.js
@@ -1,11 +1,7 @@
 import {Component} from "preact";
 import Axios from 'axios';
 import {HOST} from "../../configs";
-import {route} from "preact-router";
-
-const CATEGORIES = ["Adulto", "Artes", "Blogs", "Negócios", "Computadores", "Educacao", "Jogos", "Saúde", "Casa", "Crianças",
-    "Notícias", "Entreterimento", "Ciência", "Compras", "Sociedade", "Esportes", "Mundo", "Musica", "Religioso", "Viagem",
-    "Tecnologia"];
+import {CATEGORIES_PT} from "../utils/WebsiteCategory";
 
 export default class AddWebsite extends Component {
     constructor(props) {
@@ -14,6 +10,8 @@ export default class AddWebsite extends Component {
         this.state = {
             error: undefined
         };
+
+        this.categories = CATEGORIES_PT;
 
         this.selectedCheckBox = 0;
 
@@ -36,8 +34,8 @@ export default class AddWebsite extends Component {
         this.fields.description().value = "";
 
         this.selectedCheckBox = 0;
-        Array.from(document.getElementById("addWebsiteCategories").querySelectorAll("input[type='checkbox']")).forEach((checkbox)=>{
-           checkbox.checked = false;
+        this.getCategoriesInputs().forEach((checkbox) => {
+            checkbox.checked = false;
         });
     }
 
@@ -50,14 +48,15 @@ export default class AddWebsite extends Component {
         formData.append('url', this.fields.url().value);
         formData.append('logoURL', this.fields.logoUrl().value);
         formData.append('description', this.fields.description().value);
+        formData.append('categories', this.getCheckedCategories());
 
         Axios.post(`${HOST}/api/v1/websites`, formData, {
             headers: {
                 'Content-Type': 'multipart/form-data',
             }
         }).then(() => {
-            route('/dashboard/websites');
-            this.props.reload();
+            /*route('/dashboard/websites');
+            this.props.reload();*/
         });
     }
 
@@ -93,6 +92,26 @@ export default class AddWebsite extends Component {
         } else {
             this.selectedCheckBox > 1 ? this.selectedCheckBox-- : this.selectedCheckBox = 0;
         }
+    }
+
+    /**
+     * @return {Array} Array of categories id
+     */
+    getCheckedCategories() {
+        let ids = [];
+
+        this.getCategoriesInputs().forEach((checkbox) => {
+            ids.push(checkbox.id);
+        });
+
+        return ids;
+    }
+
+    /**
+     * @return {Array}
+     */
+    getCategoriesInputs(){
+        return Array.from(document.getElementById("addWebsiteCategories").querySelectorAll("input[type='checkbox']:checked"));
     }
 
     render({}, {error}) {
@@ -136,8 +155,9 @@ export default class AddWebsite extends Component {
                         <div class="form-group">
                             <label>Categorias</label>
                             <div id="addWebsiteCategories">
-                                {CATEGORIES.sort().map((category) => (
-                                    <Category name={category} onClickCb={this.handleCheckBoxClick.bind(this)}/>
+                                {Object.entries(this.categories).map((category) => (
+                                    <Category name={category[1]} catId={category[0]}
+                                              onClickCb={this.handleCheckBoxClick.bind(this)}/>
                                 ))}
                             </div>
                         </div>
@@ -155,10 +175,10 @@ export default class AddWebsite extends Component {
     }
 }
 
-let Category = ({name, onClickCb}) => (
+let Category = ({name, catId, onClickCb}) => (
     <div class="custom-control custom-checkbox custom-control-inline websites-add__checkbox">
-        <input class="custom-control-input" type="checkbox" value="" id={`${name}-id`} onClick={onClickCb}/>
-        <label class="custom-control-label" for={`${name}-id`}>
+        <input class="custom-control-input" type="checkbox" value="" id={catId} onClick={onClickCb}/>
+        <label class="custom-control-label" for={catId}>
             {name}
         </label>
     </div>
---
+++ frontend/src/components/utils/WebsiteCategory.js
@@ -0,0 +1,27 @@
+/*
+* The keys must be in singular
+* */
+
+export const CATEGORIES_PT = {
+    "ADULT": "Adulto",
+    "ART": "Artes",
+    "BLOG": "Blog",
+    "HOME": "Casa",
+    "SCIENCE": "Ciência",
+    "SHOPPING": "Compras",
+    "COMPUTER": "Computadores",
+    "CHILD": "Crianças",
+    "EDUCATION": "Educacao",
+    "ENTERTAINMENT": "Entreterimento",
+    "SPORT": "Esportes",
+    "GAME": "Jogos",
+    "WORLD": "Mundo",
+    "MUSIC": "Musica",
+    "BUSINESS": "Negócios",
+    "NEWS": "Notícias",
+    "RELIGIOUS": "Religioso",
+    "HEALTH": "Saúde",
+    "SOCIETY": "Sociedade",
+    "TECHNOLOGY": "Tecnologia",
+    "TRAVEL": "Viagem"
+};
