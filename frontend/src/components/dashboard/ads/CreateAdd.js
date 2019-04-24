a1d296382f4421541b279f97d1a309a4c0dca89e
Harystolho <haristolhogamer@gmail.com>
2019-04-24 09:02:56 -0300

    Verify Text and Image Ad fields

--- frontend/src/components/dashboard/ads/CreateAdd.js
+++ frontend/src/components/dashboard/ads/CreateAdd.js
@@ -6,9 +6,20 @@ export default class CreateAdd extends Component {
         super(props);
 
         this.state = {
-            error: null,
+            error: {},
             adType: "TEXT"
         };
+
+        this.fields = {
+            adName: () => document.getElementById("ad-name"),
+            adText: () => document.getElementById('ad-text'),
+            adBgColor: () => document.getElementById('ad-bgColor'),
+            adTextColor: () => document.getElementById('ad-textColor'),
+            adRefUrl: () => document.getElementById('ad-refUrl'),
+            adImageUrl: () => document.getElementById('ad-imageUrl')
+        };
+
+        this.ad = () => document.getElementsByClassName('ae-ad text')[0];
     }
 
     handleAdCheckbox(type) {
@@ -16,31 +27,111 @@ export default class CreateAdd extends Component {
     }
 
     updateAdBgColor(e) {
-        document.getElementsByClassName('ae-ad text')[0].style.backgroundColor = e.target.value;
+        this.ad().style.backgroundColor = e.target.value;
     }
 
-    render({}, {error}) {
+    updateAdTextColor(e) {
+        this.ad().style.color = e.target.value;
+    }
+
+    handleSubmit() {
+        switch (this.state.adType) {
+            case 'TEXT':
+                this.submitTextAd();
+                return;
+            case 'IMAGE':
+                this.submitImageAd();
+                return;
+            default:
+                return;
+        }
+    }
+
+    submitTextAd() {
+        if (!this.verifyTextAdFields())
+            return;
+
+        
+    }
+
+    verifyTextAdFields() {
+        this.setState({error: {}});
+
+        if (!this.verifyAdName())
+            return false;
+
+        if (this.fields.adText().value.trim().length < 5) {
+            this.setState({error: {...this.state.error, adText: "O texto deve conter pelo menos 5 caracteres"}});
+            return false;
+        }
+
+        if (!this.verifyRefUrl())
+            return false;
+
+        return true;
+    }
+
+    submitImageAd() {
+        if (!this.verifyImageAdFields())
+            return;
+    }
+
+    verifyImageAdFields() {
+        this.setState({error: {}});
+
+        if (!this.verifyAdName())
+            return false;
+
+        if (this.fields.adImageUrl().value.match(/(https:\/\/)|(http:\/\/)/g) === null) {
+            this.setState({error: {...this.state.error, adImage: "O URL da imagem nao e' valido"}});
+            return false;
+        }
+
+        if (!this.verifyRefUrl())
+            return false;
+
+        return true;
+    }
+
+    verifyAdName() {
+        if (this.fields.adName().value.trim().length < 5) {
+            this.setState({
+                error: {
+                    ...this.state.error,
+                    adName: "O nome do anuncio deve conter pelo menos 5 caracteres"
+                }
+            });
+            return false;
+        }
+
+        return true;
+    }
+
+    verifyRefUrl() {
+        if (this.fields.adRefUrl().value.match(/(https:\/\/)|(http:\/\/)/g) === null) {
+            this.setState({error: {...this.state.error, adRefUrl: "URL invalido"}});
+            return false;
+        }
+
+        return true;
+    }
+
+    render({}, state) {
         return (
             <div>
                 <div class="websites-add__container">
                     <div style="font-family: Raleway; font-size: 30px;">
-                        Adicionar Anúncio
-                    </div>
-
-                    <div>
-                        {error && (
-                            <div style="margin-top: 7px; margin-bottom: 14px;">
-                                <small style="color: red;">
-                                    {error}
-                                </small>
-                            </div>
-                        )}
+                        Criar Anúncio
                     </div>
 
                     <div style="margin-top: 5px;">
                         <div class="form-group websites-add__form">
                             <label>Nome</label>
                             <input id="ad-name" class="form-control" maxLength="60"/>
+                            {state.error.adName && (
+                                <small class="form-text ad-error">
+                                    {state.error.adName}
+                                </small>)}
                         </div>
 
                         <div class="form-group websites-add__form">
@@ -64,45 +155,85 @@ export default class CreateAdd extends Component {
                             </div>
                         </div>
 
-                        <div>
-                            {this.state.adType === 'TEXT' && (
-                                <div>
-                                    <div class="form-group websites-add__form">
-                                        <label>Texto</label>
-                                        <input id="ad-text" class="form-control"/>
-                                    </div>
-
-                                    <div class="form-group websites-add__form">
-                                        <label>Cor de fundo</label>
-                                        <input id="ad-bgColor" class="form-control" type="color"
-                                               style="width: 55px;height: 35px;" value="#FFD700"
-                                               onChange={this.updateAdBgColor.bind(this)}/>
-                                    </div>
+                        {this.state.adType === 'TEXT' && (
+                            <div>
+                                <div class="form-group websites-add__form">
+                                    <label>Texto</label>
+                                    <input id="ad-text" class="form-control"/>
+                                    {state.error.adText && (
+                                        <small class="form-text ad-error">
+                                            {state.error.adText}
+                                        </small>)}
                                 </div>
-                            )}
 
-                            {this.state.adType === 'IMAGE' && (
+                                <div class="form-group websites-add__form">
+                                    <label>Cor de fundo</label>
+                                    <input id="ad-bgColor" class="form-control ads-ad__color-picker" type="color"
+                                           value="#FFD700"
+                                           onChange={this.updateAdBgColor.bind(this)}/>
+                                </div>
+
+                                <div class="form-group websites-add__form">
+                                    <label>Cor do texto</label>
+                                    <input id="ad-textColor" class="form-control ads-ad__color-picker" type="color"
+                                           value="#000"
+                                           onChange={this.updateAdTextColor.bind(this)}/>
+                                </div>
+
+                                <div class="form-group websites-add__form">
+                                    <label>URL alvo do Anúncio</label>
+                                    <input id="ad-refUrl" class="form-control" placeholder="https://..."/>
+                                    {state.error.adRefUrl && (
+                                        <small class="form-text ad-error">
+                                            {state.error.adRefUrl}
+                                        </small>)}
+                                    <small class="form-text text-muted">O usuario sera redirecionado para esse link
+                                        quando o anuncio for clicado.
+                                    </small>
+                                </div>
+                            </div>
+                        )}
+
+                        {this.state.adType === 'IMAGE' && (
+                            <div>
                                 <div class="form-group websites-add__form">
                                     <label>URL da Imagem</label>
                                     <div class="input-group mb-3">
-                                        <input type="text" class="form-control" placeholder="https://..."
+                                        <input id="ad-imageUrl" type="text" class="form-control"
+                                               placeholder="https://..."
                                                aria-label="URL da imagem"/>
                                         <div class="input-group-append">
-                                            <button class="btn btn-outline-secondary" type="button" id="ad-loadImage">
+                                            <button class="btn btn-outline-secondary" type="button"
+                                                    id="ad-loadImage">
                                                 Carregar Imagem
                                             </button>
                                         </div>
                                     </div>
+                                    {state.error.adImage && (
+                                        <small class="form-text ad-error">
+                                            {state.error.adImage}
+                                        </small>)}
                                 </div>
-                            )}
-                        </div>
 
+                                <div class="form-group websites-add__form">
+                                    <label>URL alvo do Anúncio</label>
+                                    <input id="ad-refUrl" class="form-control" placeholder="https://..."/>
+                                    {state.error.adRefUrl && (
+                                        <small class="form-text ad-error">
+                                            {state.error.adRefUrl}
+                                        </small>)}
+                                    <small class="form-text text-muted">O usuario sera redirecionado para esse link
+                                        quando o anuncio for clicado.
+                                    </small>
+                                </div>
+                            </div>
+                        )}
 
                         <div class="btn"
-                             style="background-color: #156dc9; color: white; font-size: 17px; font-weight: bold;">
-                            Adicionar
+                             style="background-color: #156dc9; color: white; font-size: 17px; font-weight: bold;"
+                             onClick={this.handleSubmit.bind(this)}>
+                            Criar
                         </div>
-
                     </div>
                 </div>
             </div>
