3d152acf8d6942d5e0d5da92717d7402b98c4230
Harystolho <haristolhogamer@gmail.com>
2019-04-30 12:25:14 -0300

    Add ConfirmModal component

--- frontend/src/components/utils/Components.js
+++ frontend/src/components/utils/Components.js
@@ -1,10 +1,45 @@
+import {Component} from "preact";
+import {render as PreactRender} from "preact";
+
 const LeftArrow = ({cb}) => (
     <div style="position: absolute;">
         <img id="websiteBackIcon" src="/assets/left-arrow.png"
-        onClick={() => cb()}/>
+             onClick={() => cb()}/>
     </div>
 );
 
+class ConfirmationModal extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    close() {
+        document.getElementById("aeConfirmationModal").remove();
+    }
+
+    render({cb}) {
+        return (
+            <div class="ae-confirmation__modal" id="aeConfirmationModal">
+                <div class="ae-confirmation__modal-content">
+                    <div class="modal-header">Header</div>
+                    <div class="modal-body">body</div>
+                    <div class="modal-footer">
+                        <button class="btn btn-secondary" onClick={this.close.bind(this)}>Fechar</button>
+                        <button class="btn btn-primary">Confirmar</button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    static renderFullScreen() {
+        if (document.getElementById("aeConfirmationModal") === null)
+            PreactRender(<ConfirmationModal/>, document.getElementsByTagName("body")[0]);
+    }
+
+}
+
 export {
-    LeftArrow
+    LeftArrow,
+    ConfirmationModal
 }
