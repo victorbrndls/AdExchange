14c50ece42735bc016016a023e3421d9d921ad91
Harystolho <haristolhogamer@gmail.com>
2019-06-03 14:29:36 -0300

    Add wrapping components

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -9,7 +9,7 @@ export default class Home extends Component {
     render() {
         return (
             <div style="background-color: #f9fafc;">
-                <div class="header">
+                <div class="header shadow-sm">
                     <div class="container">
                         <div id="logo">
                             <img src="/assets/logo.png"/>
@@ -33,7 +33,9 @@ export default class Home extends Component {
                                     <h1 class="ae-home-banner__main-message">Faça seu site crescer com Adnamic</h1>
                                 </div>
                                 <div class="row">
-                                    <p>Exiba anuncios para seus cliente e pague somente pelos resultados</p>
+                                    <p>Exiba anuncios para seus cliente e pague somente pelos resultados. Exiba anuncios
+                                        para seus cliente e pague somente pelos resultados. Soapbox is the only tool you
+                                        need to record, edit, and share videos in minutes. </p>
                                 </div>
                                 <div class="row">
                                     <a class="p-3 my-3 ae-home-banner-button trans-0_3s">
@@ -49,16 +51,18 @@ export default class Home extends Component {
                             </div>
                         </div>
                     </section>
+
                     <section>
                         <div class="row">
                             <div class="col">
                                 <div class="row justify-content-center ae-home-benefits-header mb-5">
                                     <h2>Alcance os resultados desejados</h2>
                                 </div>
-                                <div class="row">
-                                    <div class="ae-home-tile col-sm-12 col-md-6 col-lg-4">
+                                <div class="row justify-content-around">
+                                    <div class="ae-home-tile col-sm-12 col-md-6 col-lg-3">
                                         <div class="tile-header">
                                             {/*Icon should be SVG*/}
+                                            <img src="https://via.placeholder.com/120x100.png"/>
                                         </div>
                                         <p class="tile-title">Aumente o número de visitas ao site</p>
                                         <p class="tile-body">Aumente o número de vendas on-line, reservas ou inscrições
@@ -66,17 +70,19 @@ export default class Home extends Component {
                                             e-mails com anúncios na Internet que direcionam as pessoas para seu
                                             site.</p>
                                     </div>
-                                    <div class="ae-home-tile col-sm-12 col-md-6 col-lg-4">
+                                    <div class="ae-home-tile col-sm-12 col-md-6 col-lg-3">
                                         <div class="tile-header">
                                             {/*Icon should be SVG*/}
+                                            <img src="https://via.placeholder.com/120x100.png"/>
                                         </div>
                                         <p class="tile-title">Aumente as visitas à loja</p>
                                         <p class="tile-body">Receba mais clientes na loja com anúncios que ajudam as
                                             pessoas a encontrar sua empresa no mapa</p>
                                     </div>
-                                    <div class="ae-home-tile col-sm-12 col-md-6 col-lg-4">
+                                    <div class="ae-home-tile col-sm-12 col-md-6 col-lg-3">
                                         <div class="tile-header">
                                             {/*Icon should be SVG*/}
+                                            <img src="https://via.placeholder.com/120x100.png"/>
                                         </div>
                                         <p class="tile-title">Aumente o número de visitas ao site</p>
                                         <p class="tile-body">Aumente o número de vendas on-line, reservas ou inscrições
