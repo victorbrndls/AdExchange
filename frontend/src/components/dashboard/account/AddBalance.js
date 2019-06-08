def36a454043d9f7d3114bcbf3015dc12c517643
Harystolho <haristolhogamer@gmail.com>
2019-06-08 19:27:07 -0300

    Improve add balance page for smaller screens

--- frontend/src/components/dashboard/account/AddBalance.js
+++ frontend/src/components/dashboard/account/AddBalance.js
@@ -6,65 +6,66 @@ export default class AddBalance extends Component {
     }
 
     render() {
+        let cardColumnCss = "col-sm-12 col-md-6 col-xl-4";
+
         return (
-            <div>
-                <div class="websites-account-header mb-5">
-                    Adicionar Saldo
+            <div class="container">
+                <div class="row mb-5">
+                    <div class="col websites-account-header">
+                        Adicionar Saldo
+                    </div>
                 </div>
 
-                <div class="container-fluid">
-                    <div class="row">
+                <div class="row justify-content-center">
 
-                        {/*### 1 ###*/}
-                        <div class="col-sm-12 col-md-6 col-lg-4">
-                            <div class="add-balance-card shadow">
-                                <div class="add-balance-card-header">
-                                    <img class="add-balance-card-header-icon" src="/assets/dollar.png"/>
-                                </div>
-                                <div class="add-balance-card-body">
-                                    <PaymentCardBody data={["23,00", "25,00"]}/>
-                                </div>
+                    {/*### 1 ###*/}
+                    <div class={cardColumnCss}>
+                        <div class="add-balance-card shadow">
+                            <div class="add-balance-card-header">
+                                <img class="add-balance-card-header-icon" src="/assets/dollar.png"/>
+                            </div>
+                            <div class="add-balance-card-body">
+                                <PaymentCardBody data={["23,00", "25,00"]}/>
                             </div>
                         </div>
-
-                        {/*### 2 ###*/}
-                        <div class="col-sm-12 col-md-6 col-lg-4">
-                            <div class="add-balance-card shadow">
-                                <div class="add-balance-card-header">
-                                    <img class="add-balance-card-header-icon" style="left: 35%;"
-                                         src="/assets/dollar.png"/>
-                                    <img class="add-balance-card-header-icon" style="left: 45%;"
-                                         src="/assets/dollar.png"/>
-                                </div>
-                                <div class="add-balance-card-body">
-                                    <PaymentCardBody data={["45,00", "50,00"]}/>
-                                </div>
-                            </div>
-                        </div>
-
-                        {/*### 3 ###*/}
-                        <div class="col-sm-12 col-md-6 col-lg-4">
-                            <div class="add-balance-card shadow">
-                                <div class="add-balance-card-header">
-                                    <img class="add-balance-card-header-icon" style="left: 40%;"
-                                         src="/assets/dollar.png"/>
-                                    <img class="add-balance-card-header-icon" style="left: 30%;"
-                                         src="/assets/dollar.png"/>
-                                    <img class="add-balance-card-header-icon" style="left: 50%;"
-                                         src="/assets/dollar.png"/>
-                                </div>
-                                <div class="add-balance-card-body">
-                                    <PaymentCardBody data={["95,00", "100,00"]}/>
-                                </div>
-                            </div>
-                        </div>
-
                     </div>
+
+                    {/*### 2 ###*/}
+                    <div class={cardColumnCss}>
+                        <div class="add-balance-card shadow">
+                            <div class="add-balance-card-header">
+                                <img class="add-balance-card-header-icon" style="left: 35%;"
+                                     src="/assets/dollar.png"/>
+                                <img class="add-balance-card-header-icon" style="left: 45%;"
+                                     src="/assets/dollar.png"/>
+                            </div>
+                            <div class="add-balance-card-body">
+                                <PaymentCardBody data={["45,00", "50,00"]}/>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/*### 3 ###*/}
+                    <div class={cardColumnCss}>
+                        <div class="add-balance-card shadow">
+                            <div class="add-balance-card-header">
+                                <img class="add-balance-card-header-icon" style="left: 40%;"
+                                     src="/assets/dollar.png"/>
+                                <img class="add-balance-card-header-icon" style="left: 30%;"
+                                     src="/assets/dollar.png"/>
+                                <img class="add-balance-card-header-icon" style="left: 50%;"
+                                     src="/assets/dollar.png"/>
+                            </div>
+                            <div class="add-balance-card-body">
+                                <PaymentCardBody data={["95,00", "100,00"]}/>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         )
     }
-
 }
 
 const PaymentCardBody = ({data}) => (
