38bbca4e4d9c7782549249661e5236d8d7f039dc
Harystolho <haristolhogamer@gmail.com>
2019-06-09 09:00:18 -0300

    Update plugins

--- frontend/src/components/utils/WebsiteCategory.js
+++ frontend/src/components/utils/WebsiteCategory.js
@@ -27,3 +27,31 @@ export const CATEGORIES_PT = {
     "TECHNOLOGY": "Tecnologia",
     "TRAVEL": "Viagem"
 };
+
+/*arts_and_entertainment
+business_and_consumer_services
+community_and_society
+computers_electronics_and_technology
+e-commerce_and_shopping
+finance
+food_and_drink
+gambling
+games
+health
+heavy_industry_and_engineering
+hobbies_and_leisure
+home_and_garden
+jobs_and_career
+law_and_government
+lifestyle
+news_and_media
+pets_and_animals
+reference_materials
+science_and_education
+sports
+travel_and_tourism
+vehicles
+adult
+throw "dwadiawidhwaihd";
+
+let const let var {};*/
