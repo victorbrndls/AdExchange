7cf35acda0918dd44e7a7f07544cd3d472d14577
Harystolho <haristolhogamer@gmail.com>
2019-06-13 20:52:36 -0300

    Add HelpItem component

--- frontend/src/components/dashboard/help/Help.js
+++ frontend/src/components/dashboard/help/Help.js
@@ -20,44 +20,57 @@ export default class Help extends Component {
 
                 <div class="row">
                     <div class="col">
-                        <dl class="help-item--container">
-                            <dt>Dashboard</dt>
-                            <dd>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. At consequatur debitis
-                                doloribus exercitationem illo ipsum itaque iure magni obcaecati perspiciatis quam
-                                quod repudiandae sed sint tempore unde, vero voluptate. Fugiat? Lorem ipsum dolor sit
-                                amet, consectetur adipisicing elit. At consequatur debitis doloribus exercitationem illo
-                                ipsum itaque iure magni obcaecati perspiciatis quam quod repudiandae sed sint tempore
-                                unde, vero voluptate. Fugiat?
-                            </dd>
-                        </dl>
+                        <HelpItem title={"Dashboard"}>
+                            <span class="help-subitem-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto autem delectus
+                                ducimus eos ex fuga fugit hic illum ipsa ipsum iste, laboriosam molestiae nisi nulla
+                                odio perferendis provident totam vel?
+                            </span>
+                        </HelpItem>
 
-                        <dl class="help-item--container">
-                            <dt>Websites</dt>
-                            <dd>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. At consequatur debitis
-                                doloribus exercitationem illo ipsum itaque iure magni obcaecati perspiciatis quam
-                                quod repudiandae sed sint tempore unde, vero voluptate. Fugiat? Lorem ipsum dolor sit
-                                amet, consectetur adipisicing elit. At consequatur debitis doloribus exercitationem illo
-                                ipsum itaque iure magni obcaecati perspiciatis quam quod repudiandae sed sint tempore
-                                unde, vero voluptate. Fugiat?
+                        <HelpItem title={"Websites"}>
+                            <dt>Tenho um website</dt>
+                            <dd class="help-subitem-margin help-subitem-text">
+                                Na página ‘Websites’ você pode adicionar/editar o seu site ao nossa lista de sites
+                                cadastrados.
+                                Construa um boa descrição, descreva qual tipo de conteúdo você posta, qual seu público
+                                alvo e o número de visualizações mensais. Será ela que convencerá outras pessoas a
+                                anunciarem em
+                                seu website.
                             </dd>
-                        </dl>
 
-                        <dl class="help-item--container">
-                            <dt>Propostas</dt>
-                            <dd>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. At consequatur debitis
-                                doloribus exercitationem illo ipsum itaque iure magni obcaecati perspiciatis quam
-                                quod repudiandae sed sint tempore unde, vero voluptate. Fugiat? Lorem ipsum dolor sit
-                                amet, consectetur adipisicing elit. At consequatur debitis doloribus exercitationem illo
-                                ipsum itaque iure magni obcaecati perspiciatis quam quod repudiandae sed sint tempore
-                                unde, vero voluptate. Fugiat?
+                            <dt>Quero anunciar em um website</dt>
+                            <dd class="help-subitem-margin help-subitem-text">
+                                Na página ‘Websites’ você encontra sites para fazer seus anúncios, leia a descrição e
+                                veja se o público alvo do site é parecido com o seu, utilize os filtros para facilitar a
+                                procura. Quando você encontrar o site em que deseja anunciar clique no botão “Fazer
+                                Proposta de Anúncio” para enviar uma proposta ao dono do site. (Para informações sobre
+                                como fazer um proposta leia o item “Proposta” abaixo)
                             </dd>
-                        </dl>
+                        </HelpItem>
+
+                        <HelpItem title={"Propostas"}>
+                            <span class="help-subitem-text">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, accusantium animi autem
+                            dolorum ea eaque eligendi eos esse, eveniet explicabo, iusto modi officiis porro
+                            praesentium quam rerum vel velit veritatis! Lorem ipsum dolor sit amet, consectetur
+                            adipisicing elit. Ab, accusantium animi autem
+                            dolorum ea eaque eligendi eos esse, eveniet explicabo, iusto modi officiis porro
+                            praesentium quam rerum vel velit veritatis!
+                            </span>
+                        </HelpItem>
+
                     </div>
                 </div>
             </div>
         )
     }
 }
+
+const HelpItem = ({title, children}) => (
+    <dl class="help-item--container">
+        <dt class="help-item--container--header">{title}</dt>
+        <dd class="help-item--container--body">
+            {children}
+        </dd>
+    </dl>
+);
