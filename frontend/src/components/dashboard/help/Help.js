7792303eb17bf8431e27e7dac21d3e87144c239d
Harystolho <haristolhogamer@gmail.com>
2019-06-21 09:07:00 -0300

    Improve Help page content

--- frontend/src/components/dashboard/help/Help.js
+++ frontend/src/components/dashboard/help/Help.js
@@ -22,23 +22,24 @@ export default class Help extends Component {
                     <div class="col">
                         <HelpItem title={"Dashboard"}>
                             <span class="help-subitem-text">
-                                Na página “Dashboard” você pode ver informações de desempenho de seus contratos.
-                                Como saber se as pessoas estão clicando em meu anúncio?, quantas pessoas viram meu
-                                anúncio?, essa página ajuda você a responder essas perguntas.
+                                Na página “Dashboard” você pode ver informações de desempenho de seus anúncios como:
+                                <ul>
+                                    <li>Número de visualizações diárias;</li>
+                                    <li>Número de cliques diários;</li>
+                                </ul>
                             </span>
                         </HelpItem>
 
                         <HelpItem title={"Websites"}>
-                            <dt>Tenho um website</dt>
+                            <dt>Para proprietários de website</dt>
                             <dd class="help-subitem-margin help-subitem-text">
-                                Na página ‘Websites’ você pode adicionar/editar o seu site ao nossa lista de
+                                Na página ‘Websites’ você pode adicionar e editar o seu site ao nossa lista de
                                 sites cadastrados. Construa um boa descrição, descreva qual tipo de conteúdo você posta,
                                 qual seu público alvo e o número de visualizações mensais. Será ela que convencerá
-                                outras
-                                pessoas a anunciarem em seu website.
+                                outras pessoas a anunciarem em seu website.
                             </dd>
 
-                            <dt>Quero anunciar em um website</dt>
+                            <dt>Para quem quer anunciar em nossos sites afiliados</dt>
                             <dd class="help-subitem-margin help-subitem-text">
                                 Na página ‘Websites’ você encontra sites para fazer seus anúncios, leia a descrição e
                                 veja se o público alvo do site é parecido com o seu, utilize os filtros para facilitar a
@@ -50,42 +51,33 @@ export default class Help extends Component {
 
                         <HelpItem title={"Propostas"}>
                             <span class="help-subitem-text">
-                                Quando uma pessoa deseja anunciar em um site, ela precisa enviar um proposta para o dono do site com informações como qual anuncio quer usar, por quanto tempo quer anunciar, qual o modo de pagamento e o valor de pagamento.
+                                Quando uma pessoa deseja anunciar em um site, ela precisa enviar um proposta para o dono
+                                do site com informações como qual anuncio quer usar, por quanto tempo quer anunciar,
+                                qual o modo de pagamento e o valor de pagamento.
                             </span>
 
-                            <dt>Como funciona o sistema de propostas</dt>
+                            <dt>Como fazer uma proposta</dt>
                             <dd class="help-subitem-margin help-subitem-text">
-                                Primeiramente uma pessoa que deseja anunciar procurará por um site na página ‘Websites’,
-                                após ter achado o site em que quer anunciar ela clicará no botão “Fazer Proposta de
-                                Anúncio”. A página de criação de propostas abrirá e a pessoa escolherá o anúncio que
-                                quer utilizar (Para informações sobre como criar um anuncio veja o item “Anúncios”
-                                abaixo), por quanto tempo quer que o anúncio apareça, o modo de pagamento e o valor do
-                                pagamento. Depois de ter preenchido essa informações ela clicará no botão “Enviar
-                                Proposta”. Nesse momento a proposta será adicionada a categoria “Enviadas” na página
-                                ‘Propostas’ da pessoa que enviou a proposta e também será enviada ao dono do website que
-                                poderá visualizá-la na pagina ‘Propostas’ na categoria “Recebidas”. O dono do website
-                                poderá escolher uma das opções:
+                                Na página ‘Websites’, procure o site em que deseja anunciar. Depois clique nele e por
+                                fim clique no botão “Fazer Proposta de Anúncio”.
+                            </dd>
+
+                            <span class="help-subitem-text">
+                                Quando o dono do website receber a proposta ela poderá escolher entre:
 
                                 <ul>
                                     <li>
                                         Rejeitar a proposta clicando no botão “Rejeitar proposta”.
                                     </li>
                                     <li>
-                                        Modificar algum valor da proposta como duração ou valor de pagamento e clicar no
-                                        botão “Enviar Revisão” que enviará a proposta novamente a quem criou-lá. A
-                                        pessoa poderá ver a proposta e enviar-la novamente caso ela concorde com
-                                        os novos valores.
+                                        Modificar a proposta e enviá-la para quem a criou, que por sua vez irá analisar e enviá-la novamente ao dono do site caso aceite os novos valores.
                                     </li>
                                     <li>
-                                        Aceitar a proposta clicando no botão “Aceitar Proposta”. Quando a proposta for
-                                        aceita pelo dono do site ela se torna um contrato que estará disponível na
-                                        página
-                                        ‘Contratos’.
+                                        Aceitar a proposta. Quando a proposta for aceita pelo dono do site ela se torna
+                                        um contrato e estará disponível na página ‘Contratos’.
                                     </li>
                                 </ul>
-
-
-                            </dd>
+                            </span>
                         </HelpItem>
 
                         <HelpItem title={"Contratos"}>
@@ -155,7 +147,8 @@ export default class Help extends Component {
 
                             <dt>Como exibir um anúncio próprio</dt>
                             <dd class="help-subitem-margin help-subitem-text">
-                                Para exibir um anúncio que você mesmo criou em seu site, entre na página ‘Spots’ e clique no botão
+                                Para exibir um anúncio que você mesmo criou em seu site, entre na página ‘Spots’ e
+                                clique no botão
                                 “Criar Spot”. Após isso deixe o campo "Contrato" vazio e selecione o anúncio que deseja
                                 exibir como anúncio reserva, depois clique em “Criar”.
                             </dd>
