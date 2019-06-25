31b99a0a806346716affc8e8b8ccfaf72005cb98
Harystolho <haristolhogamer@gmail.com>
2019-06-25 15:52:41 -0300

    Home page mobile responsible

--- frontend/src/components/home/AdvertiserHome.js
+++ frontend/src/components/home/AdvertiserHome.js
@@ -18,20 +18,24 @@ export default class AdvertiserHome extends Component {
                     <div class="ae-home-section ae-home-hero">
                         <div class="container ae-home-hero--container">
                             <div class="row">
-                                <div class="col-md-12 col-lg-6">
-                                    <div class="row ae-home-hero__text ae-home-hero__main-text mb-3">
-                                        The Holy Grail Of Productivity
+                                <div class="col-md-12 col-lg-6 ae-home-hero--left">
+                                    <div class="row mb-3 row">
+                                        <div class="col ae-home-hero__text ae-home-hero__main-text">
+                                            The Holy Grail Of Productivity
+                                        </div>
                                     </div>
-                                    <div class="row ae-home-hero__text ae-home-hero__secondary-text ml-1">
-                                        <ul class="list-unstyled">
-                                            <li class="list-diamond">Organize your projects, your way</li>
-                                            <li class="list-diamond">Track all your work in one place</li>
-                                            <li class="list-diamond">Collaborate more effectively</li>
-                                        </ul>
+                                    <div class="row ml-1 justify-content-md-center">
+                                        <div class="col ae-home-hero__text ae-home-hero__secondary-text">
+                                            <ul class="list-unstyled ae-home-hero--benefits-list">
+                                                <li class="list-diamond">Organize your projects, your way</li>
+                                                <li class="list-diamond">Track all your work in one place</li>
+                                                <li class="list-diamond">Collaborate more effectively</li>
+                                            </ul>
+                                        </div>
                                     </div>
                                 </div>
                                 <div class="col-md-12 col-lg-6 text-center">
-                                    <img src="https://i.imgur.com/gsB33qD.png"/>
+                                    <img src="https://i.imgur.com/gsB33qD.png" class="ae-home-hero--image"/>
                                 </div>
                             </div>
                             <div class="row ae-home-hero-trial--container">
@@ -43,12 +47,12 @@ export default class AdvertiserHome extends Component {
                                             </div>
                                         </div>
                                         <div class="row">
-                                            <div class="col ae-home-hero-trial--message">
+                                            <div class="col-12 col-lg-8 ae-home-hero-trial--message">
                                                 Duis et mollis ipsum. Nunc fringilla ornare urna, in luctus dui
                                                 suscipitt porta. Etiam convallis, ex quis efficitur blandit, felis arcu
                                                 tincidunt nunc, commodo malesuada purus elit ut metus.
                                             </div>
-                                            <div class="col text-right">
+                                            <div class="col-12 col-lg-4 ae-home-hero-trial--button-container">
                                                 <a class="ae-home-hero-trial--button" href="/auth?register">
                                                     Comece Agora
                                                 </a>
@@ -145,18 +149,20 @@ export default class AdvertiserHome extends Component {
 }
 
 const FeatureCard = ({imageSrc, header, message}) => (
-    <div class="col-lg-5 ae-home-features-card">
-        <div class="row mb-2">
-            <div class="col-auto">
-                <img class="ae-home-features-card--image" src={imageSrc}/>
+    <div class="col-12 col-lg-5 ">
+        <div class="ae-home-features-card">
+            <div class="row mb-2">
+                <div class="col-auto">
+                    <img class="ae-home-features-card--image" src={imageSrc}/>
+                </div>
+                <div class="col ae-home-features-card--header">
+                    {header}
+                </div>
             </div>
-            <div class="col ae-home-features-card--header">
-                {header}
-            </div>
-        </div>
-        <div class="row">
-            <div class="col ae-home-features-card--message">
-                {message}
+            <div class="row">
+                <div class="col ae-home-features-card--message">
+                    {message}
+                </div>
             </div>
         </div>
     </div>
@@ -169,7 +175,7 @@ const HowToCard = ({imageSrc, header, message}) => (
                 <img class="shadow position-relative" src={imageSrc}/>
             </div>
         </div>
-        <div class="col-md-5 col-lg-5">
+        <div class="col-md-5 col-lg-5 ae-home-how_to__card--text">
             <div class="row">
                 <div class="col ae-home-how_to__card--header">
                     {header}
