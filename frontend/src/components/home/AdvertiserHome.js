dd0cfbdbd38ca0b49987be34289122b2f87ffd99
Harystolho <haristolhogamer@gmail.com>
2019-06-25 14:36:46 -0300

    Add pricing card

--- frontend/src/components/home/AdvertiserHome.js
+++ frontend/src/components/home/AdvertiserHome.js
@@ -1,5 +1,6 @@
 import {Component} from "preact";
 import Header from "./Header";
+import Footer from "./Footer";
 
 export default class AdvertiserHome extends Component {
     constructor(props) {
@@ -8,7 +9,7 @@ export default class AdvertiserHome extends Component {
 
     render() {
         return (
-            <div>
+            <div style={{backgroundColor: '#F9F8FD'}}>
                 <div>
                     <Header/>
                 </div>
@@ -78,7 +79,7 @@ export default class AdvertiserHome extends Component {
                     <div class="ae-home-section ae-home-how_to">
                         <div class="container">
                             <div class="row mb-5">
-                                <div class="col ae-home-how_to--header">
+                                <div class="col ae-home--section-header">
                                     Como Funciona
                                 </div>
                             </div>
@@ -94,8 +95,49 @@ export default class AdvertiserHome extends Component {
 
                         </div>
                     </div>
+
+                    <div class="ae-home-section ae-home-prices">
+                        <div class="container">
+                            <div class="row mb-5">
+                                <div class="col ae-home--section-header">
+                                    Precos
+                                </div>
+                            </div>
+
+                            <div class="row justify-content-center">
+                                <div class="col-8 col-sm-7 col-md-5 col-lg-4 shadow ae-home-prices-card">
+                                    <div class="ae-home-prices-card--header mb-1">
+                                        Starter
+                                    </div>
+                                    <div class="ae-home-prices-card--price mb-3">
+                                        <span>R$</span>
+                                        <span class="ae-home-prices-card--price--number">0</span>
+                                        <span>/mês</span>
+                                    </div>
+
+                                    <div class="mb-5">
+                                        <ul class="list-unstyled ae-home-prices-card--features">
+                                            <li>Unlimited websites</li>
+                                            <li>20,000 Visits</li>
+                                            <li>Unlimited Ads</li>
+                                            <li>Free CDN</li>
+                                        </ul>
+                                    </div>
+
+                                    <div class="mb-2">
+                                        <a class="ae-home-prices-card--cta" href="/auth?register">
+                                            Comece Agora
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div>footer</div>
+
+                <div>
+                    <Footer/>
+                </div>
             </div>
         )
     }
@@ -128,10 +170,16 @@ const HowToCard = ({imageSrc, header, message}) => (
             </div>
         </div>
         <div class="col-md-5 col-lg-5">
-            <div class="row ae-home-how_to__card--header">
-                {header}
+            <div class="row">
+                <div class="col ae-home-how_to__card--header">
+                    {header}
+                </div>
             </div>
-            <div class="row ae-home-how_to__card--message">{message}</div>
+            <div class="row">
+                <div class="col ae-home-how_to__card--message">
+                    {message}
+                </div>
+            </div>
         </div>
     </div>
 )
